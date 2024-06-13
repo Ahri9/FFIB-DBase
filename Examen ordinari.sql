f704222024d270dd440a7bee613796b92e2408c9
Ahri9 <116707833+Ahri9@users.noreply.github.com>
2024-06-13 20:41:17 +0200

    Add files via upload

--- Examen ordinari.sql
+++ Examen ordinari.sql
@@ -1,21 +1,6 @@
 create database llibres;
 use llibres;
 
-CREATE TABLE categorias (
-categoria_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
-nom VARCHAR (50));
-
-CREATE TABLE llibres (
-ISBN VARCHAR(13) primary key,
-titol VARCHAR(50),
-any_publicacio date,
-num_exemplars MEDIUMINT);
-
-CREATE TABLE prestec (
-data_inici DATETIME,
-data_fi DATETIME,
-multa SMALLINT);
-
 CREATE TABLE usuaris (
 usuari_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
 nom VARCHAR(50),
@@ -28,3 +13,26 @@ CREATE TABLE autors (
 autor_id INT AUTO_INCREMENT PRIMARY KEY,
 nom VARCHAR(70),
 data_naixament DATETIME);
+
+CREATE TABLE llibres (
+ISBN TINYINT AUTO_INCREMENT primary key,
+titol VARCHAR(50),
+any_publicacio date,
+num_exemplars MEDIUMINT,
+autor_id INT,
+FOREIGN KEY (autor_id) REFERENCES autors(autor_id));
+
+CREATE TABLE categorias (
+categoria_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR (50),
+llibres_id TINYINT,
+FOREIGN KEY (llibres_id) REFERENCES llibres(ISBN));
+
+CREATE TABLE prestec (
+data_inici DATETIME,
+data_fi DATETIME,
+multa SMALLINT,
+usuari_id SMALLINT,
+llibre_id TINYINT,
+FOREIGN KEY (usuari_id) REFERENCES usuaris(usuari_id),
+FOREIGN KEY (llibre_id) REFERENCES llibres(ISBN));
